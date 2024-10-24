b6332bf37bdb21845a9916ffb77e5999aa37733f
Rakhmat Herlambang <rakhmat.herlambang16@gmail.com>
2024-10-24 11:21:11 +0700

    modul 3 dan tugas selesai

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -4,7 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+   <link rel="stylesheet" href="{{ asset('assets/css/create_user.css') }}">
+  <title>Create User</title>
 </head>
 <body>
   <h1>Create User</h1>
