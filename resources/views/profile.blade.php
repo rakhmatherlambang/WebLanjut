4e5425474e2a813a12ed5579669d3fbba365eb0b
Rakhmat Herlambang <rakhmat.herlambang16@gmail.com>
2024-10-24 10:18:11 +0700

    ubah alur asset ke publik

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dark Cyber Profile</title>
     <!-- Link ke file CSS -->
-    @vite('resources/css/profile.css')
+    <link rel="stylesheet" href="{{ asset('WebPrak/css/profile.css') }}">
 </head>
 <body>
     <div class="profile-card">
-        <img src="{{ asset('images/blkgirl.jpeg') }}" alt="Profile Picture">
+        <img src="{{ asset('WebPrak/img/blkgirl.jpeg') }}" alt="Profile Picture">
         <h2>{{ $nama }}</h2>
         <p>{{ $kelas }}</p>
         <p>{{ $npm }}</p>
